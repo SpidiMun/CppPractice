3221155651396896f9405f84a7e8ab0040829eaa
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-01-26 22:15:24 +0500

    Solution: Watermelons
    You want to write whether it is possible to divide a watermelon into halves with the same masses

---
+++ Арбузы/Арбузы/Арбузы.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Russian");
+    cout << "Введите массу арбуза:\n";
+    int Weigth;
+    cin >> Weigth;
+    while (Weigth <= 0)
+    {
+        cout << "Ошибка: масса арбуза не может быть отрицательной или равной нулю. Введите новую массу:\n";
+        cin >> Weigth;
+    }
+    cout << "Можно ли разделить арбуз на две части, масса которых - чётное число?\n";
+    if (Weigth > 3)
+    {
+        if (Weigth % 2 == 0)
+        {
+            cout << "YES";
+        }
+        else
+        {
+            cout << "NO";
+        }
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
