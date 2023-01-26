7d924c841030472ac3398a882b2571afef1460c3
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-01-26 23:06:05 +0500

    Solution: Rectangular isosceles triangle
    Draw in the console rectangular isosceles triangle using * symbols

---
+++ Прямоугольный равнобедренный треугольник/Прямоугольный равнобедренный треугольник/Прямоугольный равнобедренный треугольник.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Russian");
+    cout << "Введите длину боковых сторон-катетов:\n";
+
+    int N;
+    cin >> N;
+    while (N <= 0)
+    {
+        cout << "Ошибка: длина катета не может быть отрицательной или равной нулю. Введите положительное число:\n";
+        cin >> N;
+    }
+    N = N;
+    int i, j;
+    for (i = 1; i <= N; i++)
+    {
+        for (j = 1; j <= N - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = N+1-i; j <= N; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
