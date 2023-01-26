fcad02f3088b9ba8b174e7bbd532e2e94029bacc
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-01-26 23:07:23 +0500

    Solution: Find number
    Find a given number in the input sequence of numbers

---
+++ Поиск числа/Поиск числа/Поиск числа.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    cout << "Введите кол-во чисел в поседовательности:\n";
+    int n, i;
+    cin >> n;
+    i = 1;
+    cout << "Введите число, которое хотите найти:\n";
+    int z;
+    cin >> z;
+    bool f;
+    f = false;
+    cout << "Есть ли число " << z << " в последовательности?" << endl << "Последовательность:\n";
+    while (i <= n && f == false)
+    {
+        int a;
+        cin >> a;
+        if (a == z)
+        {
+            f = true;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    if (f == true)
+    {
+        cout << "Число нашлось!";
+    }
+    else
+    {
+        cout << "Такого числа нет!";
+    }
+}
