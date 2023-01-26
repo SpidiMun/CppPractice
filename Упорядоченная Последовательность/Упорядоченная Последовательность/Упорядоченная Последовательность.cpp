132d7576c7f8060d526dc0f6e95e4a399b0de94b
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-01-26 23:11:37 +0500

    Solution: Ordered sequence
    Find out if the input sequence of numbers is ordered

---
+++ Упорядоченная Последовательность/Упорядоченная Последовательность/Упорядоченная Последовательность.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale (0, "Russian");
+    unsigned int a, b; 
+    int count;
+    bool f;
+    f = true;
+    cout << "Введите элементы последовательности\n";
+    cin >> a;
+    int i;
+    if (a != 0)
+    {
+        cin >> b;
+        while (a != b && f == true && b != 0)
+        {
+            
+            f = f && (a < b);
+            if (f == true)
+            {
+                a = b;
+                cin >> b;
+            }
+        }
+        if (f == true)
+        {
+            cout << "Последовательность упорядочена\n";
+        }
+        else
+        {
+            cout << "Последовательность не упорядочена\n";
+        }
+    }
+    else
+    {
+        cout << "Последовательности не существует\n";
+    }
+}
