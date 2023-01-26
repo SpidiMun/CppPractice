d51eacadec9697fb229ffd0cd4d2280186b34a9a
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-01-26 23:13:35 +0500

    Solution: Isosceles triangle
    Print an isosceles triangle using * characters

---
+++ Равнобедренный треугольник/Равнобедренный треугольник/Равнобедренный треугольник.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Russian");
+    cout << "Введите длину основания треугольника:\n";
+    int N;
+    cin >> N;
+    while (N <= 0 || N % 2 == 0)
+    {
+        if (N <= 0)
+        {
+            cout << "Ошибка: длина основания не может быть отрицательной или равной нулю. Введите положительное число:\n";
+            cin >> N;
+        }
+        if (N % 2 == 0)
+        {
+            cout << "Ошибка: введите нечетное число:\n";
+            cin >> N;
+        }
+    }
+    int i, j;
+    for (i = 1; i <= N; i+=2)
+    {
+        for (j = 1; j <= (N - i) / 2; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (j = 1; j <= (N - i) / 2; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+
+    }
+}
