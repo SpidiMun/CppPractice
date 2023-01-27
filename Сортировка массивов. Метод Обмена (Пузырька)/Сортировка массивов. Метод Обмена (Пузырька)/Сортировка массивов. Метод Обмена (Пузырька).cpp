6c0e92f9d441dd13be0f095eb6ef85486b389070
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-01-27 22:58:06 +0500

    Solution: Exchange (bubble) method
    Sort an integer array using exchange (bubble) method

---
+++ Сортировка массивов. Метод Обмена (Пузырька)/Сортировка массивов. Метод Обмена (Пузырька)/Сортировка массивов. Метод Обмена (Пузырька).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Russian");
+    cout << "Дан массив:\n";
+    int A[15] = { 12, -32, 52, 56, -67, 89, 27, 34, -46, 65, 45, 1, -5, 23, -100 };
+    int i, j;
+    for (i = 0; i < 15; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    cout << "Сортируем методом Обмена (Пузырька).";
+    int y;
+    for (i = 0; i < 14; i++)
+    {
+        for (j = 14; j > i; j--)
+        {
+            if (A[j] < A[j - 1])
+            {
+                y = A[j];
+                A[j] = A[j - 1];
+                A[j - 1] = y;
+            }
+        }
+    }
+    cout << "Таким образом, получился массив:\n";
+    for (i = 0; i < 15; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    system("pause");
+}
