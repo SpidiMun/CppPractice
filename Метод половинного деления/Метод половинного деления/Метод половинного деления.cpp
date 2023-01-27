dc7f9d54082c831a30dec823912d39ad6794b514
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-01-27 22:29:36 +0500

    Solution: The half division method
    Find the intersection of the function with the x axis using the half division method

---
+++ Метод половинного деления/Метод половинного деления/Метод половинного деления.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+double f(double x)
+{
+	return (sqrt(1 - 0.4 * pow(x, 2)) - asin(x));
+}
+
+int main()
+{
+	setlocale(0, "Russian");
+	cout << "Введите рассматриваемый интервал:\n";
+	int a, b;
+	cout << "A = ";
+	cin >> a;
+	cout << "B = ";
+	cin >> b;
+	cout << "AB = [ " << a << " ; " << b << " ]\n";
+	cout << "Введите точность вычисления корня:\n";
+	double Epsilon;
+	cin >> Epsilon;
+	double x0, x1, x2;
+	x1 = a;
+	x2 = b;
+	while (abs(x1 - x2) >= Epsilon)
+	{
+		x0 = (x2 + x1) / 2;
+		if (f(x1) * f(x0) < 0)
+		{
+			x2 = x0;
+		}
+		else
+		{
+			if (f(x2) * f(x0) < 0)
+			{
+				x1 = x0;
+			}
+		}
+	}
+	cout << "Корень: ";
+	cout << x2 << " или " << x1;
+}
+
